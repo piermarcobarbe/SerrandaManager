8511b36fa3d214e17b351e5a26c5d513e450e3a1
piermarcobarbe <barbep@usi.ch>
2019-01-22 16:01:23 +0100

    Added auth middleware

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const basicAuth = require('express-basic-auth');
+const basicAuthUsers = require('./users');
+
+// basicAuthUsers = { "Test" : "testPasswd", "Test2" : "anotherPasswd"};
+
+console.log(basicAuthUsers);
+
 
 var indexRouter = require('./routes/index');
 var buttonsRouter = require('./routes/buttons');
@@ -18,6 +25,11 @@ app.use(logger(':remote-addr, :status - [:date[clf]] ":method :url" (:referrer)'
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(basicAuth({
+    "users" : basicAuthUsers,
+    challenge : true
+})
+);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
