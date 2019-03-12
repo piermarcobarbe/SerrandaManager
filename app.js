3f6682ac1c926f404f035604ac0f40a432071b05
piermarcobarbe <barbep@usi.ch>
2019-03-12 23:30:52 +0100

    If a './users.js' file is not present, then do not use auth.

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const basicAuth = require('express-basic-auth');
-const basicAuthUsers = require('./users');
+
+var basicAuthUsers;
+
+try{
+    basicAuthUsers = require('./users');
+} catch (e) {
+    basicAuthUsers = null;
+}
 const fs = require("fs");
 
 // basicAuthUsers = { "Test" : "testPasswd", "Test2" : "anotherPasswd"};
@@ -22,11 +29,19 @@ app.use(logger(':remote-addr, :status - [:date[clf]] ":method :url" (:referrer)'
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(basicAuth({
-    "users" : basicAuthUsers,
-    challenge : true
-})
-);
+
+// console.log(basicAuthUsers);
+
+if(basicAuthUsers){
+    console.log("Setting app users from ./users.js");
+    app.use(basicAuth({
+            "users" : basicAuthUsers,
+            challenge : true
+        })
+    );
+} else {
+    console.log("Basic Authentication disabled!");
+}
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
